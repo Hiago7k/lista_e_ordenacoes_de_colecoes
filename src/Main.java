8eed9492558c0983dd0c96394076347fabee0b4a
Hiago 7k <hiago7k@gmail.com>
2026-07-27 19:25:20 -0300

    criando coleção de lista de numeros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Main{
     static void main() {
-        System.out.println("teste");
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(5);
+        numeros.add(23);
+        numeros.add(11);
+        numeros.add(7);
+        numeros.add(50);
+        numeros.add(0);
+        numeros.add(101);
+        System.out.println(numeros);
+
+
     }
 };
