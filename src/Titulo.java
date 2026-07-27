4e42da324acc32f8c01647daf8e11397ba29a0b1
Hiago 7k <hiago7k@gmail.com>
2026-07-27 19:28:06 -0300

    criando a classe para implementar interface

---
+++ src/Titulo.java
@@ -0,0 +1,11 @@
+public class Titulo {
+    Private String nome;
+
+    public Private getString() {
+        return String;
+    }
+
+    public void setString(Private string) {
+        String = string;
+    }
+}
